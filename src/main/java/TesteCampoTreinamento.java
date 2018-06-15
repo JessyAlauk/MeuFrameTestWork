950a9511e670815e5b7e23c4ef335c00eac4fe31
labmobilersi <labmobilersi@gmail.com>
2018-06-15 18:21:54 -0300

    Criação da classe 'TesteCampoTreinamento'

---
+++ src/main/java/TesteCampoTreinamento.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteCampoTreinamento {
+
+	@Test
+	public void testTextField() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\jessica.alauk\\Downloads\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste de escrita");
+		Assert.assertEquals("Teste de escrita", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+		//getAttribute("value") retorna o valor do campo
+		driver.quit();
+	}
+	
+	@Test
+	public void testTextArea() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\jessica.alauk\\Downloads\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("teste\nde\nescrita");
+		Assert.assertEquals("teste\nde\nescrita", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+
+		driver.quit();
+	}
+	
+}
--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,18 +1,50 @@
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TesteGoogle {
-	public static void main(String[] args) throws InterruptedException {
+	
+	@Test
+	public void testChrome() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\jessica.alauk\\Downloads\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+//		driver.manage().window().setSize(new Dimension(1200, 765));
 		driver.get("http://www.google.com");
-		System.out.println(driver.getTitle());
-		driver.close();
+		Assert.assertEquals("Google", driver.getTitle());
+//		driver.close();
+		driver.quit();
 	}
+	
+	
+	//ta com erro no firefox
+//	@Test
+//	public void testFirefox() {
+//		System.setProperty("webdriver.gecko.driver", "C:\\Users\\jessica.alauk\\Downloads\\geckodriver-v0.20.1-win64\\geckodriver.exe");
+//		WebDriver driver = new FirefoxDriver();
+//		driver.get("http://www.google.com");
+//		Assert.assertEquals("Google", driver.getTitle());
+//		driver.close();
+//	}
+	
+//	@Test
+//	public void testInternetExplorer() {
+//		System.setProperty("webdriver.ie.driver", "C:\\Users\\jessica.alauk\\Downloads\\IEDriverServer.exe");
+//		WebDriver driver = new InternetExplorerDriver();
+//		driver.get("http://www.google.com");
+//		Assert.assertEquals("Google", driver.getTitle());
+//		driver.close();
+//	}
+	
+	
 }
